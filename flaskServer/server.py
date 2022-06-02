8f4a6854c87c273eb6fc5fc494e7d85d489c9354
Rafael Medina <rafael.diaz@konfio.mx>
2022-06-02 09:57:33 -0500

    Add Twilio

--- flaskServer/server.py
+++ flaskServer/server.py
@@ -2,6 +2,8 @@ from flask import Flask, jsonify, request
 from bson.json_util import dumps
 import json
 import pymongo
+from twilio.rest import Client 
+import os
 
 app = Flask(__name__)
 
@@ -14,39 +16,53 @@ myDB = client["Clients"]
 myCollection = myDB["clients"]
 
 def getLista():
-      lista = list(myCollection.find({}))
-      count = 0
-      with open('collection.json', 'w') as file:
-            file.write('{ "clients" : [')
-            for document in lista:
-                  count+=1
-                  file.write(dumps(document))
-                  if count == len(lista):
-                        break;
-                  else:
-                        file.write(',')
-            file.write(']}')
+    lista = list(myCollection.find({}))
+    count = 0
+    with open('collection.json', 'w') as file:
+        file.write('{ "clients" : [')
+        for document in lista:
+            count+=1
+            file.write(dumps(document))
+            if count == len(lista):
+                break;
+            else:
+                file.write(',')
+        file.write(']}')
+
+def sendMessage(number):
+    account_sid = os.environ['TWILIO_ACCOUNT_SID']
+    auth_token = os.environ['TWILIO_AUTH_TOKEN']
+    client = Client(account_sid, auth_token)
+
+    message = client.messages.create(
+                                body='Tu gas está próximo a vencer, por favor acude a tu banco más cercano a pagarlo!',
+                                from_='whatsapp:+14155238886',
+                                to='whatsapp:+521'+number
+                            )
+
+    print(message.sid)
 
 @app.route("/clients")
 
 def clients():
-      getLista()
-      x = open('collection.json')
-      data = json.load(x)
-      return data
+    getLista()
+    x = open('collection.json')
+    data = json.load(x)
+    return data
 
 
 @app.route("/sendNotif", methods = ["POST"])
 
 def sendNotif():
-      print(request.json)
-      return 'received'
+    number = request.json
+    sendMessage(number['number'])
+    return 'received'
 
 @app.route("/turnOffGas", methods = ["POST"])
 
 def turnOffGas():
-      print(request.json)
-      return 'received'
+    print(request.json)
+    return 'received'
 
 if __name__ == "__main__":
     app.run(debug=True)
